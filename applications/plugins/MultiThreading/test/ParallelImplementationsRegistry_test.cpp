2010960a7cb66df64ba1fc9b8d155c3ca5d381fe
Frederick Roy <froy@lnrobo.com>
2024-09-27 17:00:25 +0900

    fix unit test

--- applications/plugins/MultiThreading/test/ParallelImplementationsRegistry_test.cpp
+++ applications/plugins/MultiThreading/test/ParallelImplementationsRegistry_test.cpp
@@ -24,11 +24,15 @@
 #include <MultiThreading/ParallelImplementationsRegistry.h>
 #include <sofa/core/ObjectFactory.h>
 
+#include <sofa/simpleapi/SimpleApi.h>
+
 namespace multithreading
 {
 
 TEST(ParallelImplementationsRegistry, existInObjectFactory)
 {
+    sofa::simpleapi::importPlugin("Sofa.Component.SolidMechanics.FEM.Elastic");
+
     const auto implementations = ParallelImplementationsRegistry::getImplementations();
 
     for (const auto& [seq, par] : implementations)
